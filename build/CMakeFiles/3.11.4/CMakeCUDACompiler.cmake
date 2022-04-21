c86bd7a00f3c6d5c7336a8309bde1bc718a01296
Xuan Jiang <xuan1998@br013.ib.bridges2.psc.edu>
2022-04-21 14:20:40 -0400

    first-running

---
+++ build/CMakeFiles/3.11.4/CMakeCUDACompiler.cmake
@@ -0,0 +1,27 @@
+set(CMAKE_CUDA_COMPILER "/jet/packages/spack/opt/spack/linux-centos8-zen/gcc-8.3.1/cuda-11.1.1-a6ajxenobex5bvpejykhtnfut4arfpwh/bin/nvcc")
+set(CMAKE_CUDA_HOST_COMPILER "")
+set(CMAKE_CUDA_HOST_LINK_LAUNCHER "/usr/bin/g++")
+set(CMAKE_CUDA_COMPILER_ID "NVIDIA")
+set(CMAKE_CUDA_COMPILER_VERSION "11.1.105")
+set(CMAKE_CUDA_STANDARD_COMPUTED_DEFAULT "14")
+set(CMAKE_CUDA_SIMULATE_ID "")
+set(CMAKE_CUDA_SIMULATE_VERSION "")
+
+
+set(CMAKE_CUDA_COMPILER_ENV_VAR "CUDACXX")
+set(CMAKE_CUDA_HOST_COMPILER_ENV_VAR "CUDAHOSTCXX")
+
+set(CMAKE_CUDA_COMPILER_ID_RUN 1)
+set(CMAKE_CUDA_SOURCE_FILE_EXTENSIONS cu)
+set(CMAKE_CUDA_LINKER_PREFERENCE 15)
+set(CMAKE_CUDA_LINKER_PREFERENCE_PROPAGATES 1)
+
+set(CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES "/jet/packages/spack/opt/spack/linux-centos8-zen/gcc-8.3.1/cuda-11.1.1-a6ajxenobex5bvpejykhtnfut4arfpwh/targets/x86_64-linux/include")
+
+set(CMAKE_CUDA_HOST_IMPLICIT_LINK_LIBRARIES "cudadevrt;cudart_static;rt;pthread;dl")
+set(CMAKE_CUDA_HOST_IMPLICIT_LINK_DIRECTORIES "/jet/packages/spack/opt/spack/linux-centos8-zen/gcc-8.3.1/cuda-11.1.1-a6ajxenobex5bvpejykhtnfut4arfpwh/targets/x86_64-linux/lib/stubs;/jet/packages/spack/opt/spack/linux-centos8-zen/gcc-8.3.1/cuda-11.1.1-a6ajxenobex5bvpejykhtnfut4arfpwh/targets/x86_64-linux/lib")
+set(CMAKE_CUDA_HOST_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
+
+set(CMAKE_CUDA_IMPLICIT_LINK_LIBRARIES "cudadevrt;cudart_static;rt;pthread;dl;stdc++;m;gcc_s;gcc;c;gcc_s;gcc")
+set(CMAKE_CUDA_IMPLICIT_LINK_DIRECTORIES "/jet/packages/spack/opt/spack/linux-centos8-zen/gcc-8.3.1/cuda-11.1.1-a6ajxenobex5bvpejykhtnfut4arfpwh/targets/x86_64-linux/lib/stubs;/jet/packages/spack/opt/spack/linux-centos8-zen/gcc-8.3.1/cuda-11.1.1-a6ajxenobex5bvpejykhtnfut4arfpwh/targets/x86_64-linux/lib;/usr/lib/gcc/x86_64-redhat-linux/8;/usr/lib64;/lib64;/jet/packages/spack/opt/spack/linux-centos8-zen/gcc-8.3.1/cuda-11.1.1-a6ajxenobex5bvpejykhtnfut4arfpwh/lib64;/usr/lib")
+set(CMAKE_CUDA_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
--- solver.cu
+++ solver.cu
@@ -1,82 +1,109 @@
-#include <cuda.h>
-
 #include <iostream>
 #include <algorithm>
+#include <chrono>
 
-using floating_t = double;
 #define IX(i, j) ((i) + (N + 2) * (j))
-#define SWAP(x0, x)           \
-    {                         \
-        floating_t *tmp = x0; \
-        x0 = x;               \
-        x = tmp;              \
+#define SWAP(x0, x)      \
+    {                    \
+        float *tmp = x0; \
+        x0 = x;          \
+        x = tmp;         \
     }
 
 #define NUM_THREADS 256
 static int N_blks_small, N_blks_large, N_blks_bnd;
 
-__global__ void add_source(int N, floating_t *x, floating_t *s, floating_t dt)
+
+__global__ void add_source(int N, float *x, float *s, float dt)
 {
+    // int i, size = (N + 2) * (N + 2);
+    // for (i = 0; i < size; i++)
+    //     x[i] += dt * s[i];
     int const tid = threadIdx.x + blockIdx.x * blockDim.x;
     if (tid > (N + 2) * (N + 2))
         return;
     x[tid] += dt * s[tid];
-    /*
-    int i, size = (N + 2) * (N + 2);
-    for (i = 0; i < size; i++)
-        x[i] += dt * s[i];
-        */
 }
 
-__global__ void set_bnd_helper(int N, int b, floating_t *x)
+void set_bnd(int N, int b, float *x)
 {
-    int const tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid > N)
-        return;
-    const int i = tid + 1; // 1 <= i <= N
-
-    x[IX(0, i)] = b == 1 ? -x[IX(1, i)] : x[IX(1, i)];
-    x[IX(N + 1, i)] = b == 1 ? -x[IX(N, i)] : x[IX(N, i)];
-    x[IX(i, 0)] = b == 2 ? -x[IX(i, 1)] : x[IX(i, 1)];
-    x[IX(i, N + 1)] = b == 2 ? -x[IX(i, N)] : x[IX(i, N)];
-}
-void set_bnd(int N, int b, floating_t *x)
-{
-    // TODO: templatize
-    set_bnd_helper<<<N_blks_bnd, NUM_THREADS>>>(N, b, x);
-
+    int i;
+    for (i = 1; i <= N; i++)
+    {
+        x[IX(0, i)] = b == 1 ? -x[IX(1, i)] : x[IX(1, i)];
+        x[IX(N + 1, i)] = b == 1 ? -x[IX(N, i)] : x[IX(N, i)];
+        x[IX(i, 0)] = b == 2 ? -x[IX(i, 1)] : x[IX(i, 1)];
+        x[IX(i, N + 1)] = b == 2 ? -x[IX(i, N)] : x[IX(i, N)];
+    }
     x[IX(0, 0)] = 0.5 * (x[IX(1, 0)] + x[IX(0, 1)]);
     x[IX(0, N + 1)] = 0.5 * (x[IX(1, N + 1)] + x[IX(0, N)]);
     x[IX(N + 1, 0)] = 0.5 * (x[IX(N, 0)] + x[IX(N + 1, 1)]);
     x[IX(N + 1, N + 1)] = 0.5 * (x[IX(N, N + 1)] + x[IX(N + 1, N)]);
 }
 
-__global__ void diffuse_helper(int N, int b, floating_t *x, floating_t *x0, floating_t diff, floating_t dt, floating_t a)
+void project(int N, float *u, float *v, float *p, float *div, float *p_new)
 {
-    // TODO: remove computation of i and j and just have tid control everything
-    int const tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid > N * N)
-        return;
-    const int i = tid % N + 1, j = tid / N + 1;
-    x[IX(i, j)] = (x0[IX(i, j)] + a * (x[IX(i - 1, j)] + x[IX(i + 1, j)] + x[IX(i, j - 1)] + x[IX(i, j + 1)])) /
-                  (1 + 4 * a);
-}
+    int i, j, k;
+    float h;
+    h = 1.0 / N;
 
-void diffuse(int N, int b, floating_t *x, floating_t *x0, floating_t diff, floating_t dt)
-{
-    floating_t a = dt * diff * N * N;
-    for (int k = 0; k < 20; k++)
+    for (i = 1; i <= N; i++)
     {
-        diffuse_helper<<<N_blks_small, NUM_THREADS>>>(N, b, x, x0, diff, dt, a);
-        set_bnd(N, b, x);
+        for (j = 1; j <= N; j++)
+        {
+            div[IX(i, j)] = -0.5 * h * (u[IX(i + 1, j)] - u[IX(i - 1, j)] + v[IX(i, j + 1)] - v[IX(i, j - 1)]);
+            p[IX(i, j)] = 0;
+        }
     }
+    set_bnd(N, 0, div);
+    set_bnd(N, 0, p);
+    for (k = 0; k < 20; k++)
+    {
+
+        for (i = 1; i <= N; i++)
+        {
+            for (j = 1; j <= N; j++)
+            {
+                p_new[IX(i, j)] = (div[IX(i, j)] + p[IX(i - 1, j)] + p[IX(i + 1, j)] +
+                                   p[IX(i, j - 1)] + p[IX(i, j + 1)]) /
+                                  4;
+            }
+        }
+        SWAP(p, p_new);
+        set_bnd(N, 0, p);
+    }
+    for (i = 1; i <= N; i++)
+    {
+        for (j = 1; j <= N; j++)
+        {
+            u[IX(i, j)] -= 0.5 * (p[IX(i + 1, j)] - p[IX(i - 1, j)]) / h;
+            v[IX(i, j)] -= 0.5 * (p[IX(i, j + 1)] - p[IX(i, j - 1)]) / h;
+        }
+    }
+    set_bnd(N, 1, u);
+    set_bnd(N, 2, v);
 }
 
-void advect(int N, int b, floating_t *d, floating_t const *d0, floating_t const *u, floating_t const *v, floating_t dt)
+void diffuse(int N, int b, float *x, float *x0, float diff, float dt)
+{
+    int i, j, k;
+    float a = dt * diff * N * N;
+    // std::cout << "a:" << a << ", dt:" << dt << ", diff:" << diff << std::endl;
+    for (i = 1; i <= N; i++)
+    {
+        for (j = 1; j <= N; j++)
+        {
+            x[IX(i, j)] = x0[IX(i, j)] + a * (x0[IX(i + 1, j)] + x0[IX(i - 1, j)] + x0[IX(i, j + 1)] + x0[IX(i, j - 1)] - 4 * x0[IX(i, j)]);
+        }
+    }
+    set_bnd(N, b, x);
+}
+
+void advect(int N, int b, float *d, float *d0, float *u, float *v, float dt)
 {
     int i, j, i0, j0, i1, j1;
-    floating_t x, y, s0, t0, s1, t1;
-    const floating_t dt0 = dt * N;
+    float x, y, s0, t0, s1, t1, dt0;
+    dt0 = dt * N;
     for (i = 1; i <= N; i++)
     {
         for (j = 1; j <= N; j++)
@@ -106,100 +133,8 @@ void advect(int N, int b, floating_t *d, floating_t const *d0, floating_t const 
     set_bnd(N, b, d);
 }
 
-__global__ void project_helper1(int N, floating_t *u, floating_t *v, floating_t *p, floating_t *div, floating_t h)
-{
-    int const tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid > N * N)
-        return;
-    const int i = tid % N + 1, j = tid / N + 1;
-    div[IX(i, j)] = -0.5 * h * (u[IX(i + 1, j)] - u[IX(i - 1, j)] + v[IX(i, j + 1)] - v[IX(i, j - 1)]);
-    p[IX(i, j)] = 0;
-}
-__global__ void project_helper2(int N, floating_t *u, floating_t *v, floating_t *p, floating_t *div)
-{
-    int const tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid > N * N)
-        return;
-    const int i = tid % N + 1, j = tid / N + 1;
-    p[IX(i, j)] = (div[IX(i, j)] + p[IX(i - 1, j)] + p[IX(i + 1, j)] +
-                   p[IX(i, j - 1)] + p[IX(i, j + 1)]) /
-                  4;
-}
-__global__ void project_helper3(int N, floating_t *u, floating_t *v, floating_t *p, floating_t *div, floating_t h)
-{
-    int const tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid > N * N)
-        return;
-    const int i = tid % N + 1, j = tid / N + 1;
-    u[IX(i, j)] -= 0.5 * (p[IX(i + 1, j)] - p[IX(i - 1, j)]) / h;
-    v[IX(i, j)] -= 0.5 * (p[IX(i, j + 1)] - p[IX(i, j - 1)]) / h;
-}
-void project(int N, floating_t *u, floating_t *v, floating_t *p, floating_t *div)
-{
-    floating_t h = 1.0 / N;
-    /*
-    for (i = 1; i <= N; i++)
-    {
-        for (j = 1; j <= N; j++)
-        {
-            div[IX(i, j)] = -0.5 * h * (u[IX(i + 1, j)] - u[IX(i - 1, j)] + v[IX(i, j + 1)] - v[IX(i, j - 1)]);
-            p[IX(i, j)] = 0;
-        }
-    }
-    */
-    project_helper1<<<N_blks_small, NUM_THREADS>>>(N, u, v, p, div, h);
-    set_bnd(N, 0, div);
-    set_bnd(N, 0, p);
-    for (int k = 0; k < 20; k++)
-    {
-        /*
-        for (i = 1; i <= N; i++)
-        {
-            for (j = 1; j <= N; j++)
-            {
-                p[IX(i, j)] = (div[IX(i, j)] + p[IX(i - 1, j)] + p[IX(i + 1, j)] +
-                               p[IX(i, j - 1)] + p[IX(i, j + 1)]) /
-                              4;
-            }
-        }
-        */
-        project_helper2<<<N_blks_small, NUM_THREADS>>>(N, u, v, p, div);
-        set_bnd(N, 0, p);
-    }
-    /*
-    for (i = 1; i <= N; i++)
-    {
-        for (j = 1; j <= N; j++)
-        {
-            u[IX(i, j)] -= 0.5 * (p[IX(i + 1, j)] - p[IX(i - 1, j)]) / h;
-            v[IX(i, j)] -= 0.5 * (p[IX(i, j + 1)] - p[IX(i, j - 1)]) / h;
-        }
-    }
-    */
-    project_helper3<<<N_blks_small, NUM_THREADS>>>(N, u, v, p, div, h);
-    set_bnd(N, 1, u);
-    set_bnd(N, 2, v);
-}
-
-void vel_step(int N, floating_t *u, floating_t *v, floating_t *u0, floating_t *v0,
-              floating_t visc, floating_t dt)
-{
-    add_source<<<N_blks_large, NUM_THREADS>>>(N, u, u0, dt);
-    add_source<<<N_blks_large, NUM_THREADS>>>(N, v, v0, dt);
-    SWAP(u0, u);
-    diffuse(N, 1, u, u0, visc, dt);
-    SWAP(v0, v);
-    diffuse(N, 2, v, v0, visc, dt);
-    project(N, u, v, u0, v0);
-    SWAP(u0, u);
-    SWAP(v0, v);
-    advect(N, 1, u, u0, u0, v0, dt);
-    advect(N, 2, v, v0, u0, v0, dt);
-    project(N, u, v, u0, v0);
-}
-
-void dens_step(int N, floating_t *x, floating_t *x0, floating_t *u, floating_t *v, floating_t diff,
-               floating_t dt)
+void dens_step(int N, float *x, float *x0, float *u, float *v, float diff,
+               float dt)
 {
     add_source<<<N_blks_large, NUM_THREADS>>>(N, x, x0, dt);
     SWAP(x0, x);
@@ -208,44 +143,99 @@ void dens_step(int N, floating_t *x, floating_t *x0, floating_t *u, floating_t *
     advect(N, 0, x, x0, u, v, dt);
 }
 
+void vel_step(int N, float *u, float *v, float *u0, float *v0,
+              float visc, float dt, float *p_new)
+{
+    add_source<<<N_blks_large, NUM_THREADS>>>(N, u, u0, dt);
+    add_source<<<N_blks_large, NUM_THREADS>>>(N, v, v0, dt);
+    SWAP(u0, u);
+    diffuse(N, 1, u, u0, visc, dt);
+    SWAP(v0, v);
+    diffuse(N, 2, v, v0, visc, dt);
+    project(N, u, v, u0, v0, p_new);
+    SWAP(u0, u);
+    SWAP(v0, v);
+    advect(N, 1, u, u0, u0, v0, dt);
+    advect(N, 2, v, v0, u0, v0, dt);
+    project(N, u, v, u0, v0, p_new);
+}
+
 int main()
 {
-    int simulating = 1000;
-    const int N = 1000;
+    auto start_time = std::chrono::steady_clock::now();
+    int simulating = 100;
+    const int N = 100;
     const int size = (N + 2) * (N + 2);
-    N_blks_bnd = (N + NUM_THREADS - 1) / NUM_THREADS;
-    N_blks_small = (N * N + NUM_THREADS - 1) / NUM_THREADS;
-    N_blks_large = (size + NUM_THREADS - 1) / NUM_THREADS;
-    /*
-    floating_t static u[size]{}, v[size]{};
-    floating_t static u_prev[size]{}; // = {[0 ... 15] = 1000.0};
-    floating_t static v_prev[size]{}; // = {[0 ... 15] = 1000.0};
-    floating_t static dens[size]{}, dens_prev[size]{};
+    float static u[size], v[size];
+    float static u_prev[size]; // = {[0 ... 15] = 1000.0};
+    float static v_prev[size]; // = {[0 ... 15] = 1000.0};
+    float static dens[size], dens_prev[size];
+    float static p_new[size];
 
     std::fill(u_prev, u_prev + size, 100.0);
     std::fill(v_prev, v_prev + size, 100.0);
     std::fill(dens_prev, dens_prev + size, 100.0);
-    */
-    floating_t *u, *v, *u_prev, *v_prev, *dens, *dens_prev;
-    cudaMalloc((void **)&u, size * sizeof(floating_t));
-    cudaMalloc((void **)&v, size * sizeof(floating_t));
-    cudaMalloc((void **)&u_prev, size * sizeof(floating_t));
-    cudaMalloc((void **)&v_prev, size * sizeof(floating_t));
-    cudaMalloc((void **)&dens, size * sizeof(floating_t));
-    cudaMalloc((void **)&dens_prev, size * sizeof(floating_t));
+    float dt = 0.01;
+    float visc = 0.00001;
+    float diff = 0.00001;
 
-    /* fill commands here */
+    float* d_u;
+    float* d_v;
+    float* d_u_prev;
+    float* d_v_prev;
+    float* d_dens;
+    float* d_dens_prev;
+    float* d_p_new;
 
-    constexpr floating_t dt = 0.01;
-    constexpr floating_t visc = 0.1;
-    constexpr floating_t diff = 1;
+    cudaMalloc((void **)&d_u, size * sizeof(float));
+    cudaMalloc((void **)&d_v, size * sizeof(float));
+    cudaMalloc((void **)&d_u_prev, size * sizeof(float));
+    cudaMalloc((void **)&d_v_prev, size * sizeof(float));
+    cudaMalloc((void **)&d_dens, size * sizeof(float));
+    cudaMalloc((void **)&d_dens_prev, size * sizeof(float));
+    cudaMalloc((void **)&d_p_new, size * sizeof(float));
+
+
+
+    cudaMemcpy(d_u, u, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_v, v, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_u_prev, u_prev, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_v_prev, v_prev, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_dens, dens, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_dens_prev, dens_prev, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_p_new, p_new, size * sizeof(float), cudaMemcpyHostToDevice);
+
     while (simulating--)
     {
         // get_from_UI(dens_prev, u_prev, v_prev);
-        vel_step(N, u, v, u_prev, v_prev, visc, dt);
-        dens_step(N, dens, dens_prev, u, v, diff, dt);
+        vel_step(N, d_u, d_v, d_u_prev, d_v_prev, visc, dt, d_p_new);
+        dens_step(N, d_dens, d_dens_prev, d_u, d_v, diff, dt);
+        cudaDeviceSynchronize();
         using namespace std;
-        cout << u[5] << endl;
-        // draw_dens(N, dens);
+        //cout << u[5] << endl;
+        //  draw_dens(N, dens);
     }
+
+    cudaMemcpy(u, d_u, size * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(v, d_v, size * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(u_prev, d_u_prev, size * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(v_prev, d_v_prev, size * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(dens, d_dens, size * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(dens_prev, d_dens_prev, size * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(p_new, d_p_new, size * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaDeviceSynchronize();
+
+    cudaFree(d_u);
+    cudaFree(d_v);
+    cudaFree(d_u_prev);
+    cudaFree(d_v_prev);
+    cudaFree(d_dens);
+    cudaFree(d_dens_prev);
+    cudaFree(d_p_new);
+
+    auto end_time = std::chrono::steady_clock::now();
+    std::chrono::duration<double> difference = end_time - start_time;
+    double seconds = difference.count();
+    std::cout << "Simulation Time = " << seconds << " seconds for " << N << " blocks.\n";
 }
