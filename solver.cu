921d42f36c87655ab21c4a462f5010cfbe3cf74c
Xuan Jiang <xuan1998@br012.ib.bridges2.psc.edu>
2022-05-04 08:14:35 -0400

    finally working

--- solver.cu
+++ solver.cu
@@ -5,22 +5,32 @@
 #include <stdio.h>
 
 #define IX(i, j) ((i) + (N + 2) * (j))
-
-
-#define NUM_THREADS 256
-
-__device__ void SWAP(float *x0, float *x) 
-{
+#define SWAP(x0, x)      \
+    {                    \
         float *tmp = x0; \
         x0 = x;          \
         x = tmp;         \
-}
+    }
 
-__device__ void inline add_source(int N, float *x, float *s, float dt)
+#define NUM_THREADS 256
+
+// __device__ void SWAP(float *x0, float *x) 
+// {
+//         float *tmp = x0; \
+//         x0 = x;          \
+//         x = tmp;         \
+// }
+
+__global__ void inline add_source(int N, float *x, float *s, float dt)
 {
-    int i, size = (N + 2) * (N + 2);
-    for (i = 0; i < size; i++)
-        x[i] += dt * s[i];
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    // int i, size = (N + 2) * (N + 2);
+    if (tid < (N + 2) * (N + 2)) 
+    {
+        x[tid] += dt * s[tid];
+    }
+    // for (i = 0; i < size; i++)
+    //     x[i] += dt * s[i];
 }
 
 // __global__ void set_bnd_helper(int N, int b, float *x) {
@@ -34,10 +44,11 @@ __device__ void inline add_source(int N, float *x, float *s, float dt)
 //     }
 // }
 
-__device__ void inline set_bnd(int N, int b, float *x)
+
+__global__ void inline set_bnd(int N, int b, float *x)
 {
-    // int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    int i;
+    int i = threadIdx.x + blockIdx.x * blockDim.x + 1;
+    // int i;
 
     // set_bnd_helper<<<(N + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, b, x);
     
@@ -49,13 +60,30 @@ __device__ void inline set_bnd(int N, int b, float *x)
     //     x[IX(i, N + 1)] = b == 2 ? -x[IX(i, N)] : x[IX(i, N)];
     //     __syncthreads();
     // }
-    for (i = 1; i <= N; i++)
+    if (i <= N)
     {
         x[IX(0, i)] = b == 1 ? -x[IX(1, i)] : x[IX(1, i)];
         x[IX(N + 1, i)] = b == 1 ? -x[IX(N, i)] : x[IX(N, i)];
         x[IX(i, 0)] = b == 2 ? -x[IX(i, 1)] : x[IX(i, 1)];
-        x[IX(i, N + 1)] = b == 2 ? -x[IX(i, N)] : x[IX(i, N)];
+        x[IX(i, N + 1)] = b == 2 ? -x[IX(i, N)] : x[IX(i, N)];       
     }
+
+    // for (i = 1; i <= N; i++)
+    // {
+    //     x[IX(0, i)] = b == 1 ? -x[IX(1, i)] : x[IX(1, i)];
+    //     x[IX(N + 1, i)] = b == 1 ? -x[IX(N, i)] : x[IX(N, i)];
+    //     x[IX(i, 0)] = b == 2 ? -x[IX(i, 1)] : x[IX(i, 1)];
+    //     x[IX(i, N + 1)] = b == 2 ? -x[IX(i, N)] : x[IX(i, N)];
+    // }
+
+    // x[IX(0, 0)] = 0.5 * (x[IX(1, 0)] + x[IX(0, 1)]);
+    // x[IX(0, N + 1)] = 0.5 * (x[IX(1, N + 1)] + x[IX(0, N)]);
+    // x[IX(N + 1, 0)] = 0.5 * (x[IX(N, 0)] + x[IX(N + 1, 1)]);
+    // x[IX(N + 1, N + 1)] = 0.5 * (x[IX(N, N + 1)] + x[IX(N + 1, N)]);
+}
+
+__global__ void inline set_bnd_finish(int N, int b, float*x) 
+{
     x[IX(0, 0)] = 0.5 * (x[IX(1, 0)] + x[IX(0, 1)]);
     x[IX(0, N + 1)] = 0.5 * (x[IX(1, N + 1)] + x[IX(0, N)]);
     x[IX(N + 1, 0)] = 0.5 * (x[IX(N, 0)] + x[IX(N + 1, 1)]);
@@ -136,8 +164,10 @@ void inline project(int N, float *u, float *v, float *p, float *div, float *p_ne
     //         p[IX(i, j)] = 0;
     //     }
     // }
-    set_bnd(N, 0, div);
-    set_bnd(N, 0, p);
+    set_bnd<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 0, div);
+    set_bnd_finish<<<1, 1>>>(N, 0, div);
+    set_bnd<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 0, p);
+    set_bnd_finish<<<1, 1>>>(N, 0, p);
     for (k = 0; k < 20; k++)
     {
 
@@ -150,7 +180,8 @@ void inline project(int N, float *u, float *v, float *p, float *div, float *p_ne
         // __syncthreads();
         projectHelper2<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, div, p, p_new);
         SWAP(p, p_new);
-        set_bnd(N, 0, p);
+        set_bnd<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 0, p);
+        set_bnd_finish<<<1, 1>>>(N, 0, p);
     }
 
     // if (i <= N && j <= N) {
@@ -168,91 +199,166 @@ void inline project(int N, float *u, float *v, float *p, float *div, float *p_ne
     //         v[IX(i, j)] -= 0.5 * (p[IX(i, j + 1)] - p[IX(i, j - 1)]) / h;
     //     }
     // }
-    set_bnd(N, 1, u);
-    set_bnd(N, 2, v);
+    set_bnd<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 1, u);
+    set_bnd_finish<<<1, 1>>>(N, 1, u);
+    set_bnd<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 2, v);
+    set_bnd_finish<<<1, 1>>>(N, 2, v);
 }
 
-__device__ void inline diffuse(int N, int b, float *x, float *x0, float diff, float dt)
+// void set_bnd(int N, int b, float *x)
+// {
+//     // int tid = threadIdx.x + blockIdx.x * blockDim.x;
+//     int i;
+
+//     // set_bnd_helper<<<(N + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, b, x);
+    
+//     // int i = threadIdx.x + blockIdx.x * blockDim.x + 1;
+//     // if (i <= N) {
+//     //     x[IX(0, i)] = b == 1 ? -x[IX(1, i)] : x[IX(1, i)];
+//     //     x[IX(N + 1, i)] = b == 1 ? -x[IX(N, i)] : x[IX(N, i)];
+//     //     x[IX(i, 0)] = b == 2 ? -x[IX(i, 1)] : x[IX(i, 1)];
+//     //     x[IX(i, N + 1)] = b == 2 ? -x[IX(i, N)] : x[IX(i, N)];
+//     //     __syncthreads();
+//     // }
+//     for (i = 1; i <= N; i++)
+//     {
+//         x[IX(0, i)] = b == 1 ? -x[IX(1, i)] : x[IX(1, i)];
+//         x[IX(N + 1, i)] = b == 1 ? -x[IX(N, i)] : x[IX(N, i)];
+//         x[IX(i, 0)] = b == 2 ? -x[IX(i, 1)] : x[IX(i, 1)];
+//         x[IX(i, N + 1)] = b == 2 ? -x[IX(i, N)] : x[IX(i, N)];
+//     }
+//     x[IX(0, 0)] = 0.5 * (x[IX(1, 0)] + x[IX(0, 1)]);
+//     x[IX(0, N + 1)] = 0.5 * (x[IX(1, N + 1)] + x[IX(0, N)]);
+//     x[IX(N + 1, 0)] = 0.5 * (x[IX(N, 0)] + x[IX(N + 1, 1)]);
+//     x[IX(N + 1, N + 1)] = 0.5 * (x[IX(N, N + 1)] + x[IX(N + 1, N)]);
+// }
+
+
+__global__ void inline diffuse(int N, int b, float *x, float *x0, float diff, float dt)
 {
-    int i, j, k;
+    // int i, j, k;
     float a = dt * diff * N * N;
     // std::cout << "a:" << a << ", dt:" << dt << ", diff:" << diff << std::endl;
-    for (i = 1; i <= N; i++)
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int i = tid % N + 1;
+    int j = tid / N + 1;
+    if (i <= N && j <= N) 
     {
-        for (j = 1; j <= N; j++)
-        {
-            x[IX(i, j)] = x0[IX(i, j)] + a * (x0[IX(i + 1, j)] + x0[IX(i - 1, j)] + x0[IX(i, j + 1)] + x0[IX(i, j - 1)] - 4 * x0[IX(i, j)]);
-        }
+      x[IX(i, j)] = x0[IX(i, j)] + a * (x0[IX(i + 1, j)] + x0[IX(i - 1, j)] + x0[IX(i, j + 1)] + x0[IX(i, j - 1)] - 4 * x0[IX(i, j)]);  
     }
-    set_bnd(N, b, x);
+    // for (i = 1; i <= N; i++)
+    // {
+    //     for (j = 1; j <= N; j++)
+    //     {
+    //         x[IX(i, j)] = x0[IX(i, j)] + a * (x0[IX(i + 1, j)] + x0[IX(i - 1, j)] + x0[IX(i, j + 1)] + x0[IX(i, j - 1)] - 4 * x0[IX(i, j)]);
+    //     }
+    // }
+    // set_bnd(N, b, x);
 }
+// remember: for all the diffuse, after that you need to set_bnd
 
-__device__ void inline advect(int N, int b, float *d, float *d0, float *u, float *v, float dt)
+__global__ void inline advect(int N, int b, float *d, float *d0, float *u, float *v, float dt)
 {
-    int i, j, i0, j0, i1, j1;
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int i = tid % N + 1;
+    int j = tid / N + 1;
+    int i0, j0, i1, j1;
     float x, y, s0, t0, s1, t1, dt0;
     dt0 = dt * N;
-    for (i = 1; i <= N; i++)
+    if (i <= N && j <= N) 
     {
-        for (j = 1; j <= N; j++)
-        {
-            x = i - dt0 * u[IX(i, j)];
-            y = j - dt0 * v[IX(i, j)];
-            if (x < 0.5)
-                x = 0.5;
-            if (x > N + 0.5)
-                x = N + 0.5;
-            i0 = (int)x;
-            i1 = i0 + 1;
-            if (y < 0.5)
-                y = 0.5;
-            if (y > N + 0.5)
-                y = N + 0.5;
-            j0 = (int)y;
-            j1 = j0 + 1;
-            s1 = x - i0;
-            s0 = 1 - s1;
-            t1 = y - j0;
-            t0 = 1 - t1;
-            d[IX(i, j)] = s0 * (t0 * d0[IX(i0, j0)] + t1 * d0[IX(i0, j1)]) +
-                          s1 * (t0 * d0[IX(i1, j0)] + t1 * d0[IX(i1, j1)]);
-        }
+        x = i - dt0 * u[IX(i, j)];
+        y = j - dt0 * v[IX(i, j)];
+        if (x < 0.5)
+            x = 0.5;
+        if (x > N + 0.5)
+            x = N + 0.5;
+        i0 = (int)x;
+        i1 = i0 + 1;
+        if (y < 0.5)
+            y = 0.5;
+        if (y > N + 0.5)
+            y = N + 0.5;
+        j0 = (int)y;
+        j1 = j0 + 1;
+        s1 = x - i0;
+        s0 = 1 - s1;
+        t1 = y - j0;
+        t0 = 1 - t1;
+        d[IX(i, j)] = s0 * (t0 * d0[IX(i0, j0)] + t1 * d0[IX(i0, j1)]) +
+                        s1 * (t0 * d0[IX(i1, j0)] + t1 * d0[IX(i1, j1)]);        
     }
-    set_bnd(N, b, d);
+    // for (i = 1; i <= N; i++)
+    // {
+    //     for (j = 1; j <= N; j++)
+    //     {
+    //         x = i - dt0 * u[IX(i, j)];
+    //         y = j - dt0 * v[IX(i, j)];
+    //         if (x < 0.5)
+    //             x = 0.5;
+    //         if (x > N + 0.5)
+    //             x = N + 0.5;
+    //         i0 = (int)x;
+    //         i1 = i0 + 1;
+    //         if (y < 0.5)
+    //             y = 0.5;
+    //         if (y > N + 0.5)
+    //             y = N + 0.5;
+    //         j0 = (int)y;
+    //         j1 = j0 + 1;
+    //         s1 = x - i0;
+    //         s0 = 1 - s1;
+    //         t1 = y - j0;
+    //         t0 = 1 - t1;
+    //         d[IX(i, j)] = s0 * (t0 * d0[IX(i0, j0)] + t1 * d0[IX(i0, j1)]) +
+    //                       s1 * (t0 * d0[IX(i1, j0)] + t1 * d0[IX(i1, j1)]);
+    //     }
+    // }
+    // set_bnd(N, b, d);
 }
 
-__global__ void inline des_step_gpu(int N, float *x, float *x0, float *u, float *v, float diff,
-               float dt)
-               {
-                   
-               }
+// remember: for all the advect, after that you need to set_bnd
+
 
 void inline dens_step(int N, float *x, float *x0, float *u, float *v, float diff,
                float dt)
 {
-    add_source(N, x, x0, dt);
+    int size = (N + 2) * (N + 2);
+    add_source<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, x, x0, dt);
     SWAP(x0, x);
-    diffuse(N, 0, x, x0, diff, dt);
+    diffuse<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 0, x, x0, diff, dt);
+    set_bnd<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 0, x);
+    set_bnd_finish<<<1, 1>>>(N, 0, x);
     SWAP(x0, x);
-    advect(N, 0, x, x0, u, v, dt);
+    advect<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 0, x, x0, u, v, dt);
+    set_bnd<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 0, x);
+    set_bnd_finish<<<1, 1>>>(N, 0, x);
 }
 
 void inline vel_step(int N, float *u, float *v, float *u0, float *v0,
               float visc, float dt, float *p_new)
 {
-    add_source(N, u, u0, dt);
-    add_source(N, v, v0, dt);
-    SWAP(u0, u);
-    diffuse(N, 1, u, u0, visc, dt);
-    SWAP(v0, v);
-    diffuse(N, 2, v, v0, visc, dt);
     const int size = (N + 2) * (N + 2);
+    add_source<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, u, u0, dt);
+    add_source<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, v, v0, dt);
+    SWAP(u0, u);
+    diffuse<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 1, u, u0, visc, dt);
+    set_bnd<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 1, u);
+    set_bnd_finish<<<1, 1>>>(N, 1, u);
+    SWAP(v0, v);
+    diffuse<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 2, v, v0, visc, dt);
+    set_bnd<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 2, v);
+    set_bnd_finish<<<1, 1>>>(N, 2, v);   
     project(N, u, v, u0, v0, p_new);
     // project(N, u, v, u0, v0, p_new);
     SWAP(u0, u);
     SWAP(v0, v);
-    advect(N, 1, u, u0, u0, v0, dt);
-    advect(N, 2, v, v0, u0, v0, dt);
+    advect<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 1, u, u0, u0, v0, dt);
+    set_bnd<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 1, u);
+    set_bnd_finish<<<1, 1>>>(N, 1, u);   
+    advect<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 2, v, v0, u0, v0, dt);
+    set_bnd<<<(size + NUM_THREADS - 1) / NUM_THREADS, NUM_THREADS>>>(N, 2, v);
+    set_bnd_finish<<<1, 1>>>(N, 2, v);  
     project(N, u, v, u0, v0, p_new);
     // project(N, u, v, u0, v0, p_new);
 }
